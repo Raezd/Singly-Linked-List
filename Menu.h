dfd7979db38aaee4d877e20e12c2bb8aa1b0eab6
Raezd <31376680+Raezd@users.noreply.github.com>
2022-05-19 20:00:13 -0700

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,5 +1,78 @@
 #pragma once
-class Menu
-{
-};
 
+#include <string>
+
+using namespace std;
+
+
+    class Menu {
+
+//      =======
+        public:
+//      =======
+
+//      ============
+//      Constructors
+//      ============
+
+//         ============
+
+            Menu(void);
+
+//         ============
+
+//      ================
+//      End Constructors
+//      ================
+
+//      ===========
+//      Destructors
+//      ===========
+
+//          ============
+
+            ~Menu(void);
+
+//          ============
+
+//      ===============
+//      End Destructors
+//      ===============
+
+//      ============================
+//      Public M-Function Prototypes
+//      ============================
+
+//          ==============================
+            MenuChoices Get(); // Accessor
+            void Set(MenuChoices); // Mutator
+            void Display();
+            void QueryUser();
+            bool Continue();
+            void ProcessCommand(SinglyLinkedList&);
+//          =======================================
+// 
+//      ================================
+//      End Public M-Function Prototypes
+//      ================================
+
+
+//      ==========
+//      End public
+//      ==========
+
+
+//      ========
+        private:
+//      ========
+
+//      ================
+//      Member Variables
+//      ================
+
+//          ==============================
+            MenuChoices userMenuSelection;
+//          ==============================
+
+    }; // Class Menu
+//  ================
